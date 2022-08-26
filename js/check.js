ccf332375bcbfc29950ba2e8f949aa61c68db5b6
kartar0041 <parduman67300@gmail.com>
2022-08-26 16:25:48 +0530

    cheer faad

---
+++ js/check.js
@@ -0,0 +1,121 @@
+// document.write("hey!<br>");
+// document.write("hey! bro<br>");
+// document.write("<b>hey! brother</b><br>");
+
+
+// var z = "Hello brother";
+// document.write(z);
+
+
+// var a = 10;
+// var b = 4;
+// a **= b;
+// document.write('<br>');
+// document.write(a);
+
+
+// var x = 10;
+// console.time("test");
+// console.log([1,11,2]);
+// console.log([1,11,2]);
+// console.log([1,11,2]);
+// console.log([1,11,2]);
+// console.log([1,11,2]);
+// console.log([1,11,2]);
+// console.timeEnd("test");
+
+// var a = 10;
+// var b = 20;
+
+// if(a < 20){
+
+// }
+
+// let age = 20;
+
+// // if (age >= 18 && age <= 22) { console.log("yes you are eligible");}
+
+// var name = "Ram Ram";
+// var gender = "ma";
+
+// if (gender == "male"){
+// 	document.write("Thakur Saab" + name); 
+// }else{
+// 	document.write("Ram Ram");
+// }
+
+
+var per = prompt("What is your Percentage:");
+
+if (per >= 80 && per <= 100) {
+	document.write("Merit<br>");
+}else if (per >= 60 && per <= 80) {
+	document.write("Good<br>");
+}else if (per >= 40 && per <= 60) {
+	document.write("fair<br>");
+}else if (per >= 33 && per <= 40) {
+	document.write("Passed<br>");
+}else if(per < 33){
+	document.write("Failed<br>");
+}else{
+	document.write("please enter valid data<br>")
+}
+
+
+// let d =;
+// let f;
+
+// f = (d > 10)? "true" : "false";
+// document.write(f);
+// document.write("<br>")
+
+
+// let day = 1;
+
+// switch(day){
+// 	case 1: 
+// 		document.write("Monday");
+// 	break;
+// 	case 2:
+// 		document.write("Tuesday");
+// 	break;
+// 	case 3:
+// 		document.write("Wednesday");
+// 	break;
+// 	case 4:
+// 		document.write("Thursday");
+// 	break;
+// 	case 5:
+// 		document.write("Friday");
+// 	break;
+// 	case 6:
+// 		document.write("Saturday");
+// 	break;
+// 	case 7:
+// 		document.write("Sunday");
+// 	break;
+// 	default:
+// 		document.write("please enter valid data");
+// 	break;
+
+// }
+
+//  let a = confirm("Do you like me");
+
+// if (a){
+// 	document.write("i like you too");
+// }else{
+// 	document.write("Ok fine")
+// }
+
+
+
+// prompt("what is your Name");
+
+
+
+
+
+
+
+
